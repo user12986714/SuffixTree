1d57863e739f7b36b048f624d6778b2fb905e36f
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-08-28 16:09:36 -0400

    Fix argument error bug
    The __suffix_tree_create! function has been changed to take 4 arguments.
    However, the wrapper at lib/suffix_tree.rb still supplies only one
    argument, causing the bug.

--- lib/suffix_tree.rb
+++ lib/suffix_tree.rb
@@ -1,8 +1,8 @@
 require "suffix_tree/suffix_tree"
 
 class SuffixTree
-  VERSION = "0.1.0"
-  def self.create!(path)
-    __suffix_tree_create! path
+  VERSION = "0.1.1"
+  def self.create!(str_path, tag_path, child_path, node_path)
+    __suffix_tree_create! str_path, tag_path, child_path, node_path
   end
 end
